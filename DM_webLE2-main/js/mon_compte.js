9943344398110610ca0f3f04e11069be617b6e65
marieddddi <124163576+marieddddi@users.noreply.github.com>
2023-12-20 15:09:28 +0100

    Add files via upload

---
+++ DM_webLE2-main/js/mon_compte.js
@@ -0,0 +1,119 @@
+var jCroix = $("<div>").append(
+  '<span class="croix ui-icon ui-icon-closethick">'
+);
+
+$(document).ready(function () {
+  var couleurModifiable = false;
+  var color;
+  var selectedColors = [];
+  $(".color").each(function () {
+    selectedColors.push($(this).css("background-color"));
+  });
+  console.log(selectedColors);
+  // Cacher la liste des choix de couleurs au chargement de la page
+  $("#choix_couleurs").hide();
+  $("#validercouleur").hide();
+
+  // Gestion du clic sur le bouton "Modifier"
+  $("#changercouleur").click(function () {
+    console.log("modif");
+    // Afficher ou masquer la liste des choix de couleurs
+    couleurModifiable = true;
+    $("#choix_couleurs").show();
+    $("#changercouleur").hide();
+    $("#validercouleur").show();
+  });
+
+  // Gestion du clic sur le bouton "Valider"
+  $("#validercouleur").click(function () {
+    console.log("valider");
+    // Afficher ou masquer la liste des choix de couleurs
+    $("#choix_couleurs").hide();
+    $("#validercouleur").hide();
+    $("#changercouleur").show();
+    couleurModifiable = false;
+    var hexColors = selectedColors.map(function (color) {
+      return rgbToHex(color);
+    });
+
+    var colorsString = hexColors.join(",");
+
+    $.ajax({
+      type: "GET",
+      url: "templates/modifier_couleurs.php", // URL du script PHP pour la mise à jour en base de données
+      data: { colors: colorsString }, // Envoyer les couleurs sous forme de paramètre
+      success: function (response) {
+        console.log("Couleurs mises à jour en base de données !");
+        console.log(colorsString);
+        // Gérer la réponse du serveur si nécessaire
+      },
+    });
+  });
+
+  // Gestion du clic sur une couleur de la liste de choix
+
+  $(".color").click(function () {
+    if (couleurModifiable === true) {
+      $(".coulcoul").click(function () {
+        color = $(this).css("background-color"); // Récupérer la couleur de fond de l'élément cliqué
+      });
+      $(this).css("background-color", color);
+      var index = $(".color").index(this);
+      selectedColors[index] = color;
+      console.log(selectedColors);
+    }
+  });
+
+  $(document).on("mouseover", ".table-container", function () {
+    // Apparition de la croix
+    console.log("mouseover");
+    ele = $(this).data("affichage");
+    idSmiley = $(this).data("id");
+    jCroix.insertAfter($(this));
+  });
+
+  $(document).on("click", ".croix", function () {
+    // Suppression de la table-container
+    console.log("click");
+    //recup l'id de la table
+    console.log("Affichage récupéré : ", ele);
+    chaineSupp = ele.join(",");
+    console.log("chaineSupp", chaineSupp);
+    $.ajax({
+      type: "GET",
+      url: "templates/modifier_couleurs.php", // URL du script PHP pour la mise à jour en base de données
+      data: { affichage: chaineSupp }, // Envoyer les couleurs sous forme de paramètre
+      success: function (response) {
+        console.log("Smiley supprimé en base de données !");
+
+        // Gérer la réponse du serveur si nécessaire
+      },
+    });
+
+    // Utilisation de la fonction supprimer_smiley
+    // Faites un appel AJAX pour supprimer la table-container correspondant à cet utilisateur
+  });
+
+  let tableauClique = false; // Variable pour suivre si le tableau a été cliqué ou non
+
+  $(document).on("click", "#DupliquerSmiley", function () {
+    if (!tableauClique) {
+      // Si le tableau n'a pas encore été cliqué
+      $("#choix").html("Veuillez cliquer sur le smiley à dupliquer");
+      tableauClique = true; // Mettez la variable à true pour indiquer que le tableau a été cliqué
+    }
+  });
+
+  $(document).on("click", ".table-container", function () {
+    if (tableauClique) {
+      // Si le tableau a déjà été cliqué
+      $("#choix").html("");
+      chaine = $(this).data("affichage");
+      var chaineDup = chaine.join(",");
+      $("#chaineCouleurs").val(chaine);
+      console.log(chaineDup);
+      $("#formChaine").submit();
+      tableauClique = false; // Réinitialisez la variable pour la prochaine fois
+    }
+  });
+});
---
+++ DM_webLE2-main/templates/login.php
@@ -0,0 +1,54 @@
+<?php
+
+// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
+if (basename($_SERVER["PHP_SELF"]) != "index.php") {
+	header("Location:../index.php?view=login");
+	die("");
+}
+
+// Chargement eventuel des données en cookies
+$login = valider("login", "COOKIE");
+$passe = valider("passe", "COOKIE");
+
+?>
+
+
+
+<body>
+	<div id="corps">
+		<div id="image">
+			<img src="ressources/compte.jpg" />
+		</div>
+		<p id="mess"> <strong>Connexion</strong></p>
+		<p id="indication">Connectez-vous pour accéder à votre palette de couleurs et vos anciens smileys.</p>
+
+		<div id="formLogin">
+			<form action="controleur.php" method="GET">
+				<div class="log"><label for="login"> Adresse mail ou pseudo <span class="rouge">*</span></label> <br>
+					<input type="text" id="login" name="login" placeholder="Adresse mail ou pseudo"
+						value="<?php echo $login; ?>" />
+				</div><br />
+				<div class="log"><label for="passe">Mot de passe <span class="rouge">*</span></label><br>
+					<input type="password" id="login" name="passe" placeholder="Mot de passe"
+						value="<?php echo $passe; ?>" />
+				</div><br />
+				<div class="log"><span class="rouge">obligatoire*</span></div>
+				<div id="bouton">
+					<div id="coco">
+						<input type="submit" name="action" value="Se connecter" id="compte" />
+					</div>
+					<div id="pascompte">
+						Vous n'avez pas encore de compte ? <br /><a href="index.php?view=creer_compte">
+							Inscrivez-vous</a>
+					</div>
+				</div>
+				<!--si on a un message dans l'url, on l'affiche sur la page: -->
+				<?php if (isset($_GET['msg'])) {
+					// Récupérer la valeur du paramètre 'msg'
+					$message = urldecode($_GET['msg']); // Décoder les caractères spéciaux dans le message
+					echo '<div id="message">' . $message . '</div>';
+				} ?>
+			</form>
+		</div>
+</body>
+
